bb7e6336ebf51e84fe71bf57e2c80158e82b6906
AdrianTodt <adriantodt.ms@gmail.com>
2022-12-31 19:55:55 -0300

    Added GitHub Actions, Admin Job

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const logger = createLogger('Application');
 const url = process.env.WEBHOOK_URL!;
+const adminUrl = process.env.WEBHOOK_ADMIN_URL!;
 
 async function sendQotdWebhook(qotd: string) {
   const data = {
@@ -35,6 +36,34 @@ async function job() {
   }
 }
 
+async function adminJob() {
+  if (!adminUrl) {
+    return;
+  }
+
+  let date = new Date().toISOString().split("T")[0];
+  try {
+    await fs.access(`qotd/${date}.txt`);
+  } catch (error: any) {
+    if (error?.code === "ENOENT") {
+      const data = {
+        "content": null,
+        "embeds": [
+          {
+            "title": "Warning - Question of the Day",
+            "description": `There's no question of the day for today (${date}).\n(The file \`qotd/${date}.txt\` does not exist.)`,
+            "color": 16751360,
+            "timestamp": new Date().toISOString(),
+          }
+        ],
+        "attachments": []
+      };
+
+      await axios.post(adminUrl, data);
+    }
+  }
+}
+
 async function main() {
   if (!url) {
     logger.error('WEBHOOK_URL is not defined');
@@ -43,6 +72,11 @@ async function main() {
 
   const cron = new CronJob("0 12 * * *", job, null, true, "America/Sao_Paulo");
   cron.start();
+
+  if (adminUrl) {
+    const adminCron = new CronJob("0 8 * * *", adminJob, null, true, "America/Sao_Paulo");
+    adminCron.start();
+  }
 }
 
 main().catch((error) => {
